a41183de5ca09e2ffcb3a8372e9b216e4b6ed38a
YassinWebber <thewebber.dev@gmail.com>
2024-10-24 13:41:26 +0300

    init the app

---
+++ components/Header.tsx
@@ -0,0 +1,12 @@
+
+export default function Header() {
+    return (
+        <header
+            className="flex justify-center items-center"
+        >
+            <div className="my-5">
+                <h1 className="text-6xl font-semibold text-primry">SpeedTracker</h1>
+            </div>
+        </header>
+    );
+};
